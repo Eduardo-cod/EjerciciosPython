a96597a212274f1a8b15b9005fe13d82bbe6d597
Eduardo-cod <eduardoesparza97@hotmail.com>
2026-09-07 20:42:17 -0600

    new exercises

---
+++ 3 INTRODUCING LISTS/Exercise 3-4: Guest List/guest_list.py
@@ -0,0 +1,6 @@
+guest_list = ["Alice", "Bob", "Charlie", "David", "Eve"]
+print(f"Dear {guest_list[0]}, you are invited to dinner.")
+print(f"Dear {guest_list[1]}, you are invited to dinner.")
+print(f"Dear {guest_list[2]}, you are invited to dinner.")
+print(f"Dear {guest_list[3]}, you are invited to dinner.")
+print(f"Dear {guest_list[4]}, you are invited to dinner.")
