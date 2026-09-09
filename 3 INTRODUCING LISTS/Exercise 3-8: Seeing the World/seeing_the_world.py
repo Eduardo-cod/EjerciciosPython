92d5e8c956a4775ec481ed4cf093087ecff72ed9
Eduardo-cod <eduardoesparza97@hotmail.com>
2026-09-08 23:38:19 -0600

    add exercise 3-8 and 3-9

---
+++ 3 INTRODUCING LISTS/Exercise 3-8: Seeing the World/seeing_the_world.py
@@ -0,0 +1,26 @@
+visit_places = ['canada', 'japan', 'italy', 'australia', 'brazil']
+
+print("Original list:")
+print(visit_places)
+
+print("\nHere is the sorted list:")
+print(sorted(visit_places))
+
+print("\nHere is the original list:")
+print(visit_places)
+
+print("\nHere is the reversed list:")
+visit_places.reverse()
+print(visit_places)
+
+print("\nHere is the reversed list again:")
+visit_places.reverse()
+print(visit_places)
+
+print("\nHere is the sorted list:")
+visit_places.sort()
+print(visit_places)
+
+print("\nHere is the sorted list in reverse alphabetical order:")
+visit_places.sort(reverse=True)
+print(visit_places)
---
+++ 3 INTRODUCING LISTS/Exercise 3-9: Dinner Guests/dinner_guests.py
@@ -0,0 +1,9 @@
+guest_list = ["Alice", "Bob", "Charlie", "David", "Eve"]
+print(f"Dear {guest_list[0]}, you are invited to dinner.")
+print(f"Dear {guest_list[1]}, you are invited to dinner.")
+print(f"Dear {guest_list[2]}, you are invited to dinner.")
+print(f"Dear {guest_list[3]}, you are invited to dinner.")
+print(f"Dear {guest_list[4]}, you are invited to dinner.")
+
+# Print the number of guests invited to dinner
+print(f"\n{len(guest_list)} guests have been invited to dinner.")
