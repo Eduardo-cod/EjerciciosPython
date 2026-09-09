8e1cf12bb2da895cb0c05a2a6ced004429322dcc
Eduardo-cod <eduardoesparza97@hotmail.com>
2026-09-08 23:27:36 -0600

    changes in the cars program

--- 3 INTRODUCING LISTS/cars.py
+++ 3 INTRODUCING LISTS/cars.py
@@ -6,3 +6,18 @@ print(cars)
 # Sort the list in reverse alphabetical order
 cars.sort(reverse=True)
 print(cars)
+
+# Sort the list temporarily in alphabetical order
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+# Show that the original list is still in its original order
+print("\nHere is the original list:")
+print(cars)
+print("\nHere is the reversed list:")
+cars.reverse()
+print(cars)
+
+# Show the length of the list
+print("\nThe length of the list is:")
+print(len(cars))
