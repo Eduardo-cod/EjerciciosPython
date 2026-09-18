d78983bdf05db56eb604b08d77e27a1db6b1e200
Eduardo-cod <eduardoesparza97@hotmail.com>
2026-09-18 00:58:15 -0600

    update magicians

--- 4 WORKING WITH LISTS/magicians.py
+++ 4 WORKING WITH LISTS/magicians.py
@@ -2,5 +2,16 @@ magicians = ["alice", "david", "carolina"]
 for magician in magicians:
     print(magician)
 
+print("\n")
+
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magic show!")
+
+'''
+#muestra error Expected an indented block after 'for' statement on line 1
+for magician in magicians:
+print(magician)
+'''
